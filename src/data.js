c46865c4155e58c6edc2c4808fb0b23dd5b9b507
YajePanta <yajepanta@gmail.com>
2020-08-18 17:32:44 -0500

    función para .index., que muestra la data. cambios en main.js, está más ordenado y cada función con comentario. en gitignore se agregaron archivos para ocultar.

--- src/data.js
+++ src/data.js
@@ -7,8 +7,7 @@ const poke = data.pokemon;
 export const sortDataByNumber = (x) => x.sort((a, b) =>
 ((b.num > a.num) ? 1 : -1))
 
-export const showDataHome = (x) => x.sort((a, b) =>
-(a.num - b.num))
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -1,42 +1,73 @@
 import * as DataFunctions from './data.js';
 
 import data from './data/pokemon/pokemon.js';
-
 const pokemonData = data.pokemon; 
 
-function types (types) {
-    return `
-    <ul class="poke-types" style="list-style-type:none;">
-        ${types.map(function (type) {
-            return `<li class="poke-type-list" style="float:left;">${type}</li>`;
-        }).join('')}
-    </ul>
-    `;
-}
-
-function pokeTemplate (poke) {
-    return `
-    <div class="pokemon-card">
-        <p>${poke.num}</p>
-        <h2>${poke.name}</h2>
-        <img class="pokemon-img" src="${poke.img}">
-        ${types(poke.type)}
-    </div>
-    `;
-}
-
-document.getElementById("home-index").innerHTML =  `${pokemonData.map(pokeTemplate).join('')}`;
-  
-const btnOrderNumber = document.getElementById("btn-sort-number");
-
-btnOrderNumber.addEventListener("click", ()=>{
-    document.getElementById("home-index").innerHTML =  `${DataFunctions.sortDataByNumber(pokemonData).map(pokeTemplate).join('')}`;
+// añade el array con los tipos ordenados
+    function types (type) {
+        return `
+        <ul class="poke-types" style="list-style-type:none;">
+            ${type.map((type) => {
+                return `<li class="poke-type-list" style="float:left;">${type}</li>`;
+            }).join('')}
+        </ul>
+        `;
     }
-);
 
-const btnHome = document.getElementById("btn-home");
-
-btnHome.addEventListener("click", ()=>{
-    document.getElementById("home-index").innerHTML =  `${DataFunctions.showDataHome(pokemonData).map(pokeTemplate).join('')}`;
+// arrays con los datos de los pokemon en orden 0-9
+    function pokemonTemplate (pokemon) {
+        return `
+        <div class="pokemon-card">
+            <p>${pokemon.num}</p>
+            <h2>${pokemon.name}</h2>
+            <img class="pokemon-img" src="${pokemon.img}">
+            ${types(pokemon.type)}
+        </div>
+        `;
     }
-);
+
+// llamamos a "home", que es el espacio donde se mostrará toda la data
+    let home = document.getElementById("home-index");
+        //no funciona con let home = document.getElementById("home-index").innerHTML;
+
+// SECCIÓN: SORT BY - botón sortByNumber - botón sortByLetter
+
+    // botón sortByNumber
+    const btnSortByNumber = document.getElementById("btn-sort-number");
+
+    btnSortByNumber.addEventListener("click", sortByNumber);
+    function sortByNumber(){
+        if (home.innerHTML === ""){
+            return;
+        }
+        else { let sortedByNumber = pokemonData.map(pokemonTemplate);
+        sortedByNumber = DataFunctions.sortDataByNumber(sortedByNumber).join('');
+        home.innerHTML = `${sortedByNumber}`; }
+        /* let sortedByNumber = pokemonData.map(pokemonTemplate);
+        sortedByNumber = DataFunctions.sortDataByNumber(sortedByNumber).join('');
+        
+            if (DataFunctions.sortDataByNumber(sortedByNumber) == 1) {
+                home.innerHTML = `${sortedByNumber}`;
+            }
+            else {
+                home.innerHTML = `${sortedByNumber.reverse()}`; */
+            }
+    
+    
+ //BARRA DE NAVEGACIÓN 
+
+    // botón index, que va al inicio
+    const btnHome = document.getElementById("btn-home");
+
+    function showData() {
+        home.innerHTML =  `${pokemonData.map(pokemonTemplate).join('')}`;
+    }
+
+    btnHome.addEventListener("click", showData);
+
+// Función para limpiar toda la data:
+function clear() {
+    home.innerHTML = " ";
+                }
+
+
