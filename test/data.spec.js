4f21fa2f79cddebbe86ea92da142833b01df10d2
LauraJimenezB <laura271260@gmail.com>
2020-08-20 11:26:38 -0500

    tests

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,4 +1,4 @@
-import { sortDataByDescendingNumber, sortDataByAscendingNumber} from '../src/data.js';
+import { sortDataByDescNumber, sortDataByAscNumber, sortDataByLetA, sortDataByLetZ} from '../src/data.js';
 /* import * as main from "../src/main.js"; */
 
 export const inputTest = 
@@ -9,6 +9,14 @@ export const inputTest =
   {num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
 ];
 
+export const outputByNumAsc = 
+[
+  {num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
+  {num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
+  {num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
+  {num: '004', name: 'Charmander', type: ['Fire' ]},
+];
+
 export const outputByNumDesc = 
 [
   {num: '004', name: 'Charmander', type: ['Fire' ]},
@@ -17,24 +25,83 @@ export const outputByNumDesc =
   {num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
 ];
 
+export const outputByLetterA = 
+[
+  {num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
+  {num: '004', name: 'Charmander', type: ['Fire' ]},
+  {num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
+  {num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
+];
+
+export const outputByLetterZ = 
+[
+  {num: '003', name: 'Venusaur', type: [ 'Grass', 'Poison' ]},
+  {num: '002', name: 'Ivysaur', type: [ 'Grass', 'Poison' ]},
+  {num: '004', name: 'Charmander', type: ['Fire' ]},
+  {num: '001', name: 'Bulbasaur', type: [ 'Grass', 'Poison' ]},
+];
+
+describe('Ordenar por numero de forma ascendente', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof inputTest).toBe('object');
+  });
+
+  it('debería ser una función', () => {
+    expect(typeof sortDataByAscNumber).toBe('function');
+  });
+
+  it('debería retornar un string de los pokemons ordenado por número ascendentemente', () => {
+    expect(sortDataByAscNumber(inputTest)).toEqual(outputByNumAsc);
+  });
+});
+
 describe('Ordenar por numero de forma descendente', () => {
   it('debería ser un objeto', () => {
     expect(typeof inputTest).toBe('object');
   });
 
   it('debería ser una función', () => {
-    expect(typeof sortDataByAscendingNumber).toBe('function');
+    expect(typeof sortDataByDescNumber).toBe('function');
   });
 
   it('debería retornar un string de los pokemons ordenado por número descendentemente', () => {
-    expect(sortDataByDescendingNumber(inputTest)).toEqual(outputByNumDesc);
+    expect(sortDataByDescNumber(inputTest)).toEqual(outputByNumDesc);
   });
+});
+
+describe('Ordenar por letra de forma ascendente', () => {
+    it('debería ser un objeto', () => {
+      expect(typeof inputTest).toBe('object');
+    });
+  
+    it('debería ser una función', () => {
+      expect(typeof sortDataByLetA).toBe('function');
+    });
+  
+    it('debería retornar un string de los pokemons ordenado por letradescendentemente', () => {
+      expect(sortDataByLetA(inputTest)).toEqual(outputByLetterA);
+    });
+});
+  
+describe('Ordenar por letra de forma descendente', () => {
+      it('debería ser un objeto', () => {
+        expect(typeof inputTest).toBe('object');
+      });
+    
+      it('debería ser una función', () => {
+        expect(typeof sortDataByLetZ).toBe('function');
+      });
+    
+      it('debería retornar un string de los pokemons ordenado por letra descendentemente', () => {
+        expect(sortDataByLetZ(inputTest)).toEqual(outputByLetterZ);
+      });
+});
+
 
 /*   it.skip('debería retornar un "string"', () => {
     expect(main.pokemonTemplate(inputTest)).toBe('string');
   }) */
 
-});
 
 /*
 describe('anotherExample', () => {
@@ -45,5 +112,4 @@ describe('anotherExample', () => {
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
   });
-});
-*/
+});*/
